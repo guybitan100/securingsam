44ea81de9dfa2e46400e05a2496972faf300983b
Guy Bitan <guy.bitan@gmail.com>
2021-12-18 21:36:20 +0200

    First project

--- src/main/java/com/reputation/concurrency/ThreadWorker.java
+++ src/main/java/com/reputation/concurrency/ThreadWorker.java
@@ -1,8 +1,6 @@
-
 package com.reputation.concurrency;
 
 import com.google.gson.Gson;
-import com.reputation.conf.Configuration;
 import com.reputation.models.Metric;
 import com.reputation.rest.RestClient;
 import com.google.common.io.ByteStreams;
@@ -10,7 +8,6 @@ import com.google.common.io.CharSource;
 import com.google.common.io.CharStreams;
 import com.reputation.rest.RestClientNew;
 import org.apache.log4j.Logger;
-import org.springframework.http.HttpHeaders;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,27 +19,18 @@ import java.util.concurrent.Callable;
 
 
 public class ThreadWorker implements Callable {
-    public final Configuration conf;
     final static Logger log4j = Logger.getLogger(ThreadWorker.class);
     private List<Metric> metrics;
     private final String uri;
     private final Gson gson;
 
     public ThreadWorker(String uri) {
-        conf = new Configuration("api.properties");
         RestClient.disableSslVerification();
         this.uri = uri;
         metrics = new ArrayList<>();
         this.gson = new Gson();
     }
 
-    public synchronized RestClient getBaseLogin() {
-        RestClient apiRestClient = new RestClient(conf.get("base_url"));
-        HttpHeaders headers = apiRestClient.postEntity(conf.get("login_endpoint")).getHeaders();
-        String set_cookie = (headers.getFirst(headers.SET_COOKIE)).split(";")[0];
-        apiRestClient.addHeader("Cookie", set_cookie);
-        return apiRestClient;
-    }
 
     public String getJsonAsString(String fileName) {
         InputStream stream = getClass().getClassLoader().getResourceAsStream(fileName);
