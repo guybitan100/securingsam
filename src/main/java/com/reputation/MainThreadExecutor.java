3662072c47312b441db816365a5918a4e68f657f
Guy Bitan <guy.bitan@gmail.com>
2021-12-18 22:36:07 +0200

    First project

--- src/main/java/com/reputation/MainThreadExecutor.java
+++ src/main/java/com/reputation/MainThreadExecutor.java
@@ -1,8 +1,12 @@
 package com.reputation;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import com.reputation.concurrency.ThreadWorker;
 import org.apache.log4j.Logger;
 
+import java.io.FileReader;
+import java.net.URL;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -13,19 +17,40 @@ public class MainThreadExecutor implements Runnable {
     final static Logger log4j = Logger.getLogger(MainThreadExecutor.class);
     public int maxTreads;
     private final ExecutorService executorService;
-    private final String uri;
+    private final int maxDomains;
+    private ArrayBlockingQueue<String> abq;
 
-    public MainThreadExecutor(String uri, int maxTreads) {
+    public MainThreadExecutor(int maxDomains, int maxTreads) {
         this.maxTreads = maxTreads;
-        this.uri = uri;
-        executorService = Executors.newFixedThreadPool(maxTreads);
+        this.maxDomains = maxDomains;
+        this.executorService = Executors.newFixedThreadPool(maxTreads);
+        this.abq = new ArrayBlockingQueue<>(maxDomains);
     }
 
+    public void readAllDataAtOnce() {
+        URL resource = MainThreadExecutor.class.getClassLoader().getResource("top500Domains.csv");
+        if (resource == null) {
+            throw new IllegalArgumentException("file not found!");
+        }
+        try {
+            FileReader filereader = new FileReader(String.valueOf(resource.toURI()));
+            CSVReader csvReader = new CSVReaderBuilder(filereader)
+                    .withSkipLines(1)
+                    .build();
+            List<String[]> allData = csvReader.readAll();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
     private void runDomains() {
+        readAllDataAtOnce();
         Set<Callable<ThreadWorker>> callables = new HashSet<>();
         try {
             for (int i = 0; i < maxTreads; i++) {
-                callables.add(new ThreadWorker(uri));
+                callables.add(new ThreadWorker("uri"));
             }
             executorService.invokeAll(callables);
             executorService.shutdown();
@@ -42,6 +67,6 @@ public class MainThreadExecutor implements Runnable {
     }
 
     public static void main(String[] args) {
-        new MainThreadExecutor("https://candidate-eval.securingsam.com/domain/ranking/google.com", 100).run();
+        new MainThreadExecutor(5, 100).run();
     }
 }
